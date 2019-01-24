c5082a60c6b8076f978f855193bca60287de224a
Paloma Ania <paloma.aniabenito@gmail.com>
2019-01-24 18:18:15 +0100

    Show house shield of each character

--- src/components/CharacterInfo/index.js
+++ src/components/CharacterInfo/index.js
@@ -18,16 +18,30 @@ class CharacterInfo extends Component {
             deadOrAliveState = 'Unknown';
         }
 
+        let houseShield;
+        if (characterData.house === 'Gryffindor'){
+            houseShield = 'http://www.ruths-study.com/movies/harry-potter/gshield.jpg';
+        } else if (characterData.house === 'Ravenclaw') {
+            houseShield = 'https://musingsfromnevillesnavel.files.wordpress.com/2011/08/ravenclaw-shield1.jpg';
+        } else if (characterData.house === 'Slytherin') {
+            houseShield = 'http://www.ruths-study.com/movies/harry-potter/sshield.jpg';
+        } else if (characterData.house === 'Hufflepuff') {
+            houseShield = 'http://www.ruths-study.com/movies/harry-potter/hshield.jpg';
+        } 
+
         return ( 
-            <div className="character-container">
-                <h1>{characterData.name}</h1>
-                <img className="character-img" src={characterData.image} alt=""/>
-                <ul>
-                    <li>House: {characterData.house}</li>
-                    <li>Birth: {characterData.yearOfBirth}</li>
-                    <li>Patronus: {characterData.patronus}</li>
-                    <li>{deadOrAliveState}</li>
-                </ul>
+            <div className="principal-container-with-shield">
+                <div className="character-container-info">
+                    <h1>{characterData.name}</h1>
+                    <img className="character-img" src={characterData.image} alt=""/>
+                    <ul>
+                        <li className="character-info-li">House: {characterData.house}</li>
+                        <li className="character-info-li">Birth: {characterData.yearOfBirth}</li>
+                        <li className="character-info-li">Patronus: {characterData.patronus}</li>
+                        <li className="character-info-li">{deadOrAliveState}</li>
+                    </ul>
+                </div>
+                <img className="shield-image" src={houseShield} alt=""/>
             </div>
          );
     }
