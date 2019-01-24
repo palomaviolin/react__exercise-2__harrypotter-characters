6b6db1dbfb2a4fa4c847e0c421fc968005dd60a6
Paloma Ania <paloma.aniabenito@gmail.com>
2019-01-24 20:15:47 +0100

    Indent a fragment of code, previously disorganized

--- src/components/CharacterCard/index.js
+++ src/components/CharacterCard/index.js
@@ -1,25 +1,25 @@
-import React, { Component } from 'react';
-import './CharacterCard.scss';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import "./CharacterCard.scss";
+import { Link } from "react-router-dom";
 
-class  CharacterCard extends Component {
-    showDetailedInfo() {
-        console.log('CLicked')
-    }
+class CharacterCard extends Component {
+  showDetailedInfo() {
+    console.log("CLicked");
+  }
 
-    render() { 
-        let characterData = this.props.characterData;
-        console.log('Character chard', this.props);
-        return (
-            <Link to={`/characters/${this.props.characterId}`}>
-                <div className="character-container" onClick={this.showDetailedInfo}>
-                <h1 className="text-character-name">{characterData.name}</h1>
-                <img className="character-img" src={characterData.image} alt=""/>
-                <h2 className="text-character-house">{characterData.house}</h2>
-                </div>
-            </Link>
-         );
-    }
+  render() {
+    let characterData = this.props.characterData;
+    console.log("Character chard", this.props);
+    return (
+      <Link to={`/characters/${this.props.characterId}`}>
+        <div className="character-container" onClick={this.showDetailedInfo}>
+          <h1 className="text-character-name">{characterData.name}</h1>
+          <img className="character-img" src={characterData.image} alt="" />
+          <h2 className="text-character-house">{characterData.house}</h2>
+        </div>
+      </Link>
+    );
+  }
 }
- 
+
 export default CharacterCard;
