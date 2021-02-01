d7610acd6ad1ecc8db19848ef73e5f9df18d3a78
Paloma Ania <paloma.aniabenito@gmail.com>
2021-02-01 22:08:13 +0100

    Updates

---
+++ precache-manifest.1a39cbc16b05b9125981595c6a9a8a5a.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "e3959b85a7b43460ae2467e0edd5ba5e",
+    "url": "/react__exercise-2__harrypotter-characters/static/media/gryffindorShield.e3959b85.png"
+  },
+  {
+    "revision": "4fcc7819d84fa56970ae",
+    "url": "/react__exercise-2__harrypotter-characters/static/css/main.fc1bb1c9.chunk.css"
+  },
+  {
+    "revision": "3fc0f8f6379afe32e387",
+    "url": "/react__exercise-2__harrypotter-characters/static/js/1.3fc0f8f6.chunk.js"
+  },
+  {
+    "revision": "144248e41b2c37f44579",
+    "url": "/react__exercise-2__harrypotter-characters/static/js/runtime~main.144248e4.js"
+  },
+  {
+    "revision": "a440f8a26eacae950ed61d1023cc0ab6",
+    "url": "/react__exercise-2__harrypotter-characters/static/media/ThemeSong.a440f8a2.mp3"
+  },
+  {
+    "revision": "aa5cc295e22d762a680ccbee1b44d86b",
+    "url": "/react__exercise-2__harrypotter-characters/static/media/titleSmall.aa5cc295.png"
+  },
+  {
+    "revision": "4fcc7819d84fa56970ae",
+    "url": "/react__exercise-2__harrypotter-characters/static/js/main.4fcc7819.chunk.js"
+  },
+  {
+    "revision": "1285cde0feaedfde0199f33c6a707ded",
+    "url": "/react__exercise-2__harrypotter-characters/static/media/ravenclawShield.1285cde0.png"
+  },
+  {
+    "revision": "3c4f0f63f3d977f493b48b569ba86be7",
+    "url": "/react__exercise-2__harrypotter-characters/static/media/slytherinShield.3c4f0f63.png"
+  },
+  {
+    "revision": "952cfe6bb454b3fab74adb2d64635019",
+    "url": "/react__exercise-2__harrypotter-characters/static/media/hufflepuffShield.952cfe6b.png"
+  },
+  {
+    "revision": "77398c0e2545176b6d5ab5f758130bf5",
+    "url": "/react__exercise-2__harrypotter-characters/static/media/DoubleTroubleSong.77398c0e.mp3"
+  },
+  {
+    "revision": "1c4ad1200d6722a660073cb519c4b43c",
+    "url": "/react__exercise-2__harrypotter-characters/static/media/skyBackground.1c4ad120.jpg"
+  },
+  {
+    "revision": "d2f63e69b4e5997ba9d03f02910dc927",
+    "url": "/react__exercise-2__harrypotter-characters/index.html"
+  }
+];
